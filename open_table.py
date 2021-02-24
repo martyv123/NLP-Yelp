4c6f69c140db73d19f3901d577b5403a92caf9a6
Marty <martyv123@ccs.neu.edu>
2021-02-24 02:33:32 -0500

    more bug fixing, index error is because criteria c does not produce enough results

--- open_table.py
+++ open_table.py
@@ -566,14 +566,15 @@ def start_calculations(businesses):
         if final_output_b and final_output_c:
             for id, f in enumerate(final_to_write):
                 for i in range(10):
-                    if i < len(final_output_b) and i < len(final_output_c):
+                    if i < len(final_output_b):
                         b_score = "score_" + str(i+1) + "b"
                         b_review = "reviewed_against_" + str(i+1) + "b"
-                        c_score = "score_" + str(i+1) + "c"
-                        c_review = "reviewed_against_" + str(i+1) + "c"
-                            
                         f[b_review] = final_output_b[id][b_review]
                         f[b_score] = final_output_b[id][b_score]
+                    
+                    if i < len(final_output_c):
+                        c_score = "score_" + str(i+1) + "c"
+                        c_review = "reviewed_against_" + str(i+1) + "c"
                         f[c_review] = final_output_c[id][c_review]
                         f[c_score] = final_output_c[id][c_score]
 
